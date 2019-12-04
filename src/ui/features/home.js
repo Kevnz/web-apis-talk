11c3d8dc18523749ad8da11eac9f243446d8c930
Kevin Isom <kevin.isom@gmail.com>
2019-12-04 21:53:52 +1300

    update

--- src/ui/features/home.js
+++ src/ui/features/home.js
@@ -1,2 +1,12 @@
-import React from "react"
-export default () => <main><h1>My App</h1></main>
+import React from 'react'
+import { Section, Title, SubTitle } from '@brightleaf/elements'
+import { Link } from '@reach/router'
+export default () => (
+  <Section>
+    <Title>Web APIs</Title>
+    <SubTitle>I did not know the browser did that</SubTitle>
+    <Link to="/ambient-light" className="button is-info">
+      Start
+    </Link>
+  </Section>
+)
