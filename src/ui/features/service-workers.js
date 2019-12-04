7d4d0eeb5f8aeacd12c67c0dbdedc93bbc0c928d
Kevin Isom <kevin.isom@gmail.com>
2019-12-05 09:23:36 +1300

    service worker holder

--- src/ui/features/service-workers.js
+++ src/ui/features/service-workers.js
@@ -2,21 +2,7 @@ import React, { useReducer } from 'react'
 import { Section, Title, SubTitle } from '@brightleaf/elements'
 import { Highlighter } from 'ui/components/highlighter'
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'hidden':
-      return { visible: false, hidden: true }
-    case 'visible':
-      return { visible: true, hidden: false }
-    default:
-      return state
-  }
-}
-
-const useWebWorker = (url, data) => {}
-
 export default () => {
-  useWebWorker('/log', { action: 'demo' })
   return (
     <Section>
       <Title>Web APIs</Title>
