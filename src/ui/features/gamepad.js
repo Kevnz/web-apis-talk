54ef213b7f69de1c5b84bccf161fbfbaeb578b99
Kevin Isom <kevin.isom@gmail.com>
2019-12-04 21:58:07 +1300

    gamepad holder

---
+++ src/ui/features/gamepad.js
@@ -0,0 +1,32 @@
+import React, { useReducer } from 'react'
+import { Section, Title, SubTitle } from '@brightleaf/elements'
+import { Highlighter } from 'ui/components/highlighter'
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'hidden':
+      return { visible: false, hidden: true }
+    case 'visible':
+      return { visible: true, hidden: false }
+    default:
+      return state
+  }
+}
+
+const useGamePad = (url, data) => {}
+
+export default () => {
+  useGamePad('/log', { action: 'demo' })
+  return (
+    <Section>
+      <Title>Web APIs</Title>
+      <SubTitle>Gamepad API</SubTitle>
+      <Highlighter className="javascript" languages={['javascript']}>
+        {`
+
+
+      `}
+      </Highlighter>
+    </Section>
+  )
+}
